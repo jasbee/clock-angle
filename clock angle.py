ee4ec39a7647aa184fa73a66cf7635849ac4562d
Johnathan Asbee <johnathanasbee@gmail.com>
2016-08-26 00:09:54 -0400

    Converted to python 3 and cleaned up
    Converted to python 3 and cleaned up

--- clock angle.py
+++ clock angle.py
@@ -1,22 +1,23 @@
 def clock_angle(h, m):
-    hour = (60 * h + m)/2.0
+    hour = (60 * h + m) / 2.0
     minute = 6 * m
     angle = abs(hour - minute)
-    
+
     if angle > 180:
         angle = 360 - angle
     return angle
 
-x = raw_input('Please enter the time: ')
+x = input('Please enter the time: ')
 
 h = int(x[:-3])
 if len(x) is 5:
     m = int(x[3:])
-if len (x) is 4:
+if len(x) is 4:
     m = int(x[2:])
 
 a = int(clock_angle(h, m))
 
-print "The angle between the two clock hands at", x, "would be", a, "degrees."
+print("The angle between the two clock hands at " + x +
+" would be " + str(a) + " degrees.")
 
-raw_input('')
+input('')
